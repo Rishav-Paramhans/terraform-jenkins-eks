28fd628ebc212597507b1d12a722165b821fd6de
Rishav-Paramhans <rishavkrparamhans@gmail.com>
2025-04-06 22:26:17 +0200

    Updated policy_arn in jenkins_policy

--- EKS/main.tf
+++ EKS/main.tf
@@ -48,7 +48,7 @@ module "eks" {
       principal_arn = "arn:aws:iam::891612581521:role/jenkins-eks-iam-auth-role"
       policy_associations = {
         jenkins_policy = {
-          policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
           access_scope = {
             type       = "cluster"
             namespaces = ["default"]
