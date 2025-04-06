5e3c4a2736df1a01a86d5076dd9a15c9b8b92229
Rishav-Paramhans <rishavkrparamhans@gmail.com>
2025-04-06 22:30:59 +0200

    Removed namspace=defualt from access scope

--- EKS/main.tf
+++ EKS/main.tf
@@ -51,7 +51,6 @@ module "eks" {
           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
           access_scope = {
             type       = "cluster"
-            namespaces = ["default"]
           }
         }
       }
