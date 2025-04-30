deaf512e569654c00fd2a544594a373520e1c82b
Rishav-Paramhans <rishavkrparamhans@gmail.com>
2025-04-30 21:43:44 +0200

    Syntax error fix

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -62,19 +62,19 @@ module "eks" {
         }
       }
     }
-  }
-  rishav_access = {
-      principal_arn = "arn:aws:iam::891612581521:user/rishav-user"
-      policy_associations = {
-        rishav_policy = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = {
-            type = "cluster"
+  
+    rishav_access = {
+        principal_arn = "arn:aws:iam::891612581521:user/rishav-user"
+        policy_associations = {
+          rishav_policy = {
+            policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+            access_scope = {
+              type = "cluster"
+            }
           }
         }
       }
     }
-  }
 
 
   eks_managed_node_groups = {
