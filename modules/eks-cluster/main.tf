d33466b4afafcb6f12e7754442fa46cb0db174d4
Rishav-Paramhans <rishavkrparamhans@gmail.com>
2025-05-02 16:20:41 +0200

    Modified AMI type for ollama node group

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -114,7 +114,8 @@ module "eks" {
     }
 
     ollama = {
-      ami_type       = "amazon-eks-node-al2023-x86_64-nvidia-1.27-v20250501"             # ADD THIS LINE
+      #ami_type       = "amazon-eks-node-al2023-x86_64-nvidia-1.27-v20250501"             # ADD THIS LINE
+      ami_type       = "BOTTLEROCKET_x86_64_NVIDIA"             # ADD THIS LINE
       instance_types = ["g4dn.xlarge"] # GPU support
       desired_size   = 1
       min_size       = 1
